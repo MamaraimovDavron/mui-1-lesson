25f32c6ef7fb6fd3fda442d29cff834480ea05c9
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2024-01-01 17:40:04 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import HomeIcon from "@mui/icons-material/Home";
+import PenIcon from "@mui/icons-material/EditAttributesOutlined";
+import Delete from "@mui/icons-material/Delete";
 
-function App() {
+const Main = styled.div`
+  width: 60vw;
+  height: 50vh;
+  margin: 200px auto;
+  box-shadow: 0px 0px 10px gray;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  .topBtns {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+  .btnGroup {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .icons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Main>
+      <div className="topBtns">
+        <Button variant="text" color="primary">
+          Hello PDP !
+        </Button>
+        <Button variant="contained" color="success">
+          Hello PDP !
+        </Button>
+        <Button variant="outlined" color="error">
+          Hello PDP !
+        </Button>
+      </div>
+
+      <div className="btnGroup">
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+          color="success"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Button color="warning">Tashkent</Button>
+          <Button>New York</Button>
+          <Button color="primary">London</Button>
+        </ButtonGroup>
+      </div>
+
+      <div className="icons">
+        <Button variant="outlined" color="warning">
+          <HomeIcon />
+          Home
+        </Button>
+
+        <Button variant="contained" color="success">
+          <PenIcon />
+          Edit
+        </Button>
+
+        <Button variant="contained" color="error">
+          <Delete />
+          Delete
+        </Button>
+      </div>
+    </Main>
   );
-}
+};
 
 export default App;
